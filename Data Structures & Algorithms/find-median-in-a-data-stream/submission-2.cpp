61c060df9af3ec12d29d7eea7ff8fa1130714e72
kashish <kashish.tiwari.ug23@nsut.ac.in>
2026-04-17 19:54:26 +0530

    Add: find-median-in-a-data-stream - submission-2

---
+++ Data Structures & Algorithms/find-median-in-a-data-stream/submission-2.cpp
@@ -0,0 +1,61 @@
+class MedianFinder {
+public:
+
+
+  priority_queue<int>left;
+  priority_queue<int , vector<int> , greater<int>>right;
+
+
+  // here comes the picture which u need to keep in mind the fact 
+  // we need to maintain the half portion whicb consists of smaller
+  // elements in the left half such that yk we can clearly access the 
+  // largest element of that part and we need to maintain a smaller priority 
+  // queue such that yk we can clearly access the smallest element
+  // of the left section
+
+
+
+
+
+
+    MedianFinder() {
+        
+    }
+    
+    void addNum(int num) {
+        
+
+        if( left.empty() || num < left.top()){
+            left.push(num);
+        }
+
+        else{
+            right.push(num);
+        }
+
+        if( right.size() > left.size()){
+
+            int value = right.top();
+            right.pop();
+            left.push(value);
+        }
+
+        if (left.size() > right.size() + 1) {
+           right.push(left.top());
+           left.pop();
+        }
+    }
+    
+    double findMedian() {
+        
+
+        if( left.size() == right.size()){
+
+            return ((left.top() + right.top()))/2.0 ;
+        }
+
+        else{
+            return left.top();
+        }
+    }
+};
